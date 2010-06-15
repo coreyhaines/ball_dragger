998318d3e485f1ec4cee72d6205f38f2c48a638a
coreyhaines <coreyhaines@gmail.com>
2010-06-15 15:46:38 -0500

    Flushing out feature steps with html fixture

--- features/playing_game.feature.js
+++ features/playing_game.feature.js
@@ -1,8 +1,28 @@
-function pressStartButton(){
-}
 describe("playing the game", function() {
+  var gameBoard, field, ballSource, ballDestination, results;
+
+  function pressStartButton(){
+    gameBoard.find("#start-button").click();
+  }
+
+  beforeEach(function() {
+    gameBoard = $("#game");
+    field = gameBoard.find("#field");
+    ballSource = field.find("#ball-source");
+    ballDestination = field.find("#ball-destination");
+    results = gameBoard.find("#results");
+  });
+
   describe("pressing the start button", function() {
     it("creates a ball in the source", function() {
+      pressStartButton();
+
+      var ball = ballSource.find(".ball");
+      expect(ball.length).toEqual(1);
+    });
+
+    it("removes the old ball if pressed again", function() {
+      pressStartButton();
       pressStartButton();
 
       var ball = ballSource.find(".ball");
